7251cc99e628ae87fd36e0fc20c8c68b87b3d0ae
Marco KOK <mkok@e1z3r3p2.42.us.org>
2017-02-23 09:59:02 -0800

    updated script

--- Install_Homebrew/install_homebrew.sh
+++ Install_Homebrew/install_homebrew.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Need to implement check to see if homebrew is already installed
 cd /Volumes/Storage/goinfre/$(whoami)
 git clone https://github.com/Homebrew/homebrew.git
 echo 'alias brew="/Volumes/Storage/goinfre/$(whoami)/homebrew/bin/brew"' >> ~/.zshrc
